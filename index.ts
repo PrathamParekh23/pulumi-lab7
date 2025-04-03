c8a6ff53361831b8edfa869a33706943439cb186
Pratham <prathamparekh16@gmail.com>
2025-04-03 17:12:10 -0400

    Added GitHub Actions deploy workflow

--- index.ts
+++ index.ts
@@ -1,9 +1,63 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+// Create an S3 bucket for the website
+const bucket = new aws.s3.Bucket("my-website-bucket", {
+    website: {
+        indexDocument: "index.html",
+    },
+});
 
-// Export the name of the bucket
+// Upload a simple HTML file from a local file
+const indexObject = new aws.s3.BucketObject("index.html", {
+    bucket: bucket.id,
+    source: new pulumi.asset.FileAsset("index.html"),
+    contentType: "text/html",
+});
+
+// Add a bucket policy to make the bucket publicly readable
+const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: bucket.id,
+    policy: bucket.arn.apply(arn => JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [{
+            Effect: "Allow",
+            Principal: "*",
+            Action: ["s3:GetObject"],
+            Resource: `${arn}/*`,
+        }],
+    })),
+});
+
+// Set up CloudFront to deliver the site
+const distribution = new aws.cloudfront.Distribution("my-distribution", {
+    enabled: true,
+    origins: [{
+        domainName: bucket.bucketRegionalDomainName,
+        originId: bucket.arn,
+    }],
+    defaultRootObject: "index.html",
+    defaultCacheBehavior: {
+        targetOriginId: bucket.arn,
+        viewerProtocolPolicy: "redirect-to-https",
+        allowedMethods: ["GET", "HEAD", "OPTIONS"],
+        cachedMethods: ["GET", "HEAD"],
+        forwardedValues: {
+            queryString: false,
+            cookies: { forward: "none" },
+        },
+    },
+    priceClass: "PriceClass_100",
+    viewerCertificate: {
+        cloudfrontDefaultCertificate: true,
+    },
+    restrictions: {
+        geoRestriction: {
+            restrictionType: "none",
+        },
+    },
+});
+
+// Export the bucket name and CloudFront URL
 export const bucketName = bucket.id;
+export const cloudfrontUrl = distribution.domainName;
